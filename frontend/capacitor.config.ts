e9684122e17a6747260891e4383971379afedede
Seu Nome <betodalas@gmail.com>
2026-03-14 12:43:16 -0300

    fix

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nossahistoria.app',
+  appName: 'Nossa História',
+  webDir: 'dist'
+};
+
+export default config;
